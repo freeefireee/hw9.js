08ec60d0ea4f151b91d011ad96f9421e58a32192
Mister.X <146196540+freeefireee@users.noreply.github.com>
2023-10-28 11:56:12 +0600

    Add files via upload

---
+++ hw-9.js/code.js
@@ -0,0 +1,266 @@
+const array = [
+  {
+    createdAt: "2023-10-27T01:52:46.215Z",
+    name: "Darren Funk",
+    number: "1-318-208-5926",
+    gender: "Intersex woman",
+    age: 73,
+    img: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1154.jpg",
+    job: "Legacy Intranet Technician",
+    id: "1",
+  },
+  {
+    createdAt: "2023-10-26T20:35:53.897Z",
+    name: "Perry Kunze",
+    number: "(332) 834-1281 x17993",
+    gender: "Cisgender man",
+    age: 94,
+    img: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/978.jpg",
+    job: "Internal Operations Coordinator",
+    id: "2",
+  },
+  {
+    createdAt: "2023-10-27T05:23:33.146Z",
+    name: "Bill Will",
+    number: "708-747-6470",
+    gender: "Cis",
+    age: 17,
+    img: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/146.jpg",
+    job: "Regional Security Liaison",
+    id: "3",
+  },
+  {
+    createdAt: "2023-10-27T07:05:54.251Z",
+    name: "Leslie Koelpin",
+    number: "322-665-0125 x93948",
+    gender: "Intersex person",
+    age: 63,
+    img: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/643.jpg",
+    job: "Global Functionality Executive",
+    id: "4",
+  },
+  {
+    createdAt: "2023-10-27T02:31:07.216Z",
+    name: "Eugene West",
+    number: "1-907-276-4488",
+    gender: "Intersex man",
+    age: 17,
+    img: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/88.jpg",
+    job: "Principal Metrics Administrator",
+    id: "5",
+  },
+  {
+    createdAt: "2023-10-27T04:57:14.168Z",
+    name: "Irvin Grant",
+    number: "(837) 361-4145 x078",
+    gender: "Cis male",
+    age: 95,
+    img: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/155.jpg",
+    job: "District Usability Specialist",
+    id: "6",
+  },
+  {
+    createdAt: "2023-10-26T23:29:12.735Z",
+    name: "Carlos Collier",
+    number: "660-616-4030 x477",
+    gender: "Two* person",
+    age: 71,
+    img: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/912.jpg",
+    job: "Legacy Paradigm Planner",
+    id: "7",
+  },
+  {
+    createdAt: "2023-10-26T20:17:44.410Z",
+    name: "Rosemary Gerhold Sr.",
+    number: "598.909.4543 x414",
+    gender: "Gender nonconforming",
+    age: 62,
+    img: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/23.jpg",
+    job: "Senior Identity Facilitator",
+    id: "8",
+  },
+  {
+    createdAt: "2023-10-26T23:18:59.664Z",
+    name: "Tina Roberts",
+    number: "766-809-2687",
+    gender: "Demi-woman",
+    age: 62,
+    img: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/62.jpg",
+    job: "Direct Infrastructure Producer",
+    id: "9",
+  },
+  {
+    createdAt: "2023-10-27T06:32:26.483Z",
+    name: "Linda Dicki",
+    number: "1-271-892-1922",
+    gender: "Cisgender male",
+    age: 2,
+    img: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/734.jpg",
+    job: "National Accountability Specialist",
+    id: "10",
+  },
+  {
+    createdAt: "2023-10-26T19:30:37.781Z",
+    name: "Shelly Gibson IV",
+    number: "(368) 874-5273 x130",
+    gender: "Trigender",
+    age: 50,
+    img: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/359.jpg",
+    job: "Lead Program Director",
+    id: "11",
+  },
+  {
+    createdAt: "2023-10-27T11:53:19.026Z",
+    name: "Albert Waters",
+    number: "544.213.3029 x605",
+    gender: "M2F",
+    age: 67,
+    img: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/501.jpg",
+    job: "Investor Integration Producer",
+    id: "12",
+  },
+  {
+    createdAt: "2023-10-26T18:46:53.078Z",
+    name: "Leona Crona",
+    number: "786.437.5722 x88165",
+    gender: "Female to male transgender man",
+    age: 79,
+    img: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/41.jpg",
+    job: "Lead Metrics Assistant",
+    id: "13",
+  },
+  {
+    createdAt: "2023-10-26T18:36:27.432Z",
+    name: "Tommie Pfannerstill",
+    number: "885.984.2836",
+    gender: "Demiflux",
+    age: 38,
+    img: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/786.jpg",
+    job: "Product Factors Designer",
+    id: "14",
+  },
+  {
+    createdAt: "2023-10-26T19:44:22.092Z",
+    name: "Walter Price",
+    number: "(599) 834-2921 x8150",
+    gender: "Male to female transgender woman",
+    age: 56,
+    img: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/676.jpg",
+    job: "National Accountability Agent",
+    id: "15",
+  },
+  {
+    createdAt: "2023-10-26T19:46:33.241Z",
+    name: "Miss Loren Windler",
+    number: "(234) 794-7149",
+    gender: "Cis man",
+    age: 61,
+    img: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/693.jpg",
+    job: "Human Implementation Consultant",
+    id: "16",
+  },
+  {
+    createdAt: "2023-10-26T17:23:27.242Z",
+    name: "Marcus Wiegand",
+    number: "1-306-666-8997 x6888",
+    gender: "Gender fluid",
+    age: 91,
+    img: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/519.jpg",
+    job: "Forward Tactics Planner",
+    id: "17",
+  },
+  {
+    createdAt: "2023-10-27T07:25:48.683Z",
+    name: "Mercedes Weissnat",
+    number: "(538) 330-2114",
+    gender: "Demi-man",
+    age: 21,
+    img: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/76.jpg",
+    job: "Internal Infrastructure Associate",
+    id: "18",
+  },
+  {
+    createdAt: "2023-10-27T03:33:17.717Z",
+    name: "Justin VonRueden",
+    number: "1-738-365-1374 x3866",
+    gender: "T* man",
+    age: 37,
+    img: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/797.jpg",
+    job: "District Security Consultant",
+    id: "19",
+  },
+  {
+    createdAt: "2023-10-27T06:41:44.959Z",
+    name: "Rick Weber",
+    number: "(908) 820-0931",
+    gender: "Hermaphrodite",
+    age: 93,
+    img: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/800.jpg",
+    job: "Lead Program Orchestrator",
+    id: "20",
+  },
+];
+
+const cardList = document.getElementById("result");
+const searchInput = document.getElementById("searchInput");
+const filterButton = document.getElementById("filter");
+
+function createCard(person) {
+
+  const card = document.createElement("div");
+  card.className = "card";
+
+  const imgDiv = document.createElement("div");
+  imgDiv.className = "img";
+  const img = document.createElement("img");
+  img.src = person.img;
+  imgDiv.appendChild(img);
+
+  const nameSpan = document.createElement("span");
+  nameSpan.textContent = person.name;
+
+  const jobParagraf = document.createElement("p");
+  jobParagraf.className = "job";
+  jobParagraf.textContent = person.job;
+
+  const button = document.createElement("button");
+  button.textContent = "Click";
+
+
+  card.appendChild(imgDiv);
+  card.appendChild(nameSpan);
+  card.appendChild(jobParagraf);
+  card.appendChild(button);
+
+
+  cardList.appendChild(card);
+}
+
+function displayCards(cardsArray) {
+  cardList.innerHTML = "";
+  cardsArray.forEach((person) => {
+      createCard(person);
+  });
+}
+
+searchInput.addEventListener("input", function () {
+  const searchText = searchInput.value.trim().toLowerCase();
+
+  if (searchText === "") {
+    
+      displayCards(array);
+  } else {
+    
+      const searchResults = array.filter((person) =>
+          person.name.toLowerCase().includes(searchText)
+      );
+      displayCards(searchResults);
+  }
+});
+
+filterButton.addEventListener("click", function () {
+  const filteredResults = array.filter((person) => person.age > 30);
+  displayCards(filteredResults);
+});
+
+displayCards(array);
